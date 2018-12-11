80e5e16600baee6f3cfd68542807cb5878900e58
Martin Otyeka <otyeka.martin@gmail.com>
2018-12-11 00:30:04 -0500

    Initial Commit

---
+++ Trimmer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trimmer
+//
+//  Created by Martin Otyeka on 2018-12-11.
+//  Copyright © 2018 Capsule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
