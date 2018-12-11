f38c6e5f053b3dcbe3d20e04820ba8871c34b53b
Martin Otyeka <otyeka.martin@gmail.com>
2018-12-11 13:01:23 -0500

    Create FrameCell

---
+++ Trimmer/Trimmer/FrameCell.swift
@@ -0,0 +1,32 @@
+//
+//  FrameCell.swift
+//  Trimmer
+//
+//  Created by Martin Otyeka on 2018-12-11.
+//  Copyright © 2018 Capsule. All rights reserved.
+//
+
+import UIKit
+
+final class FrameCell: UICollectionViewCell {
+    
+    private lazy var imageView = UIImageView()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+    
+}
+
+
+extension FrameCell {
+    
+    public func configure(with frame: UIImage) {
+        self.imageView.frame = self.bounds
+        self.imageView.clipsToBounds = true
+        self.imageView.backgroundColor = .black
+        self.imageView.contentMode = .scaleAspectFill
+        self.contentView.addSubview(self.contentView)
+    }
+}
