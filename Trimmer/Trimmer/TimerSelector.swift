a05ca23354eed7009623bc82dd7f7f1698ad7ecd
Martin Otyeka <otyeka.martin@gmail.com>
2018-12-15 14:06:27 -0500

    Refactor

---
+++ Trimmer/Trimmer/TimerSelector.swift
@@ -0,0 +1,16 @@
+//
+//  TimerSelector.swift
+//  Trimmer
+//
+//  Created by Martin Otyeka on 2018-12-15.
+//  Copyright © 2018 Capsule. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+final class TimeSelector {
+    
+    private var virtualPosition: CGFloat?
+    
+}
