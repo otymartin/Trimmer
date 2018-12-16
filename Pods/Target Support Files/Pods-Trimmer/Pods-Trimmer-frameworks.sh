1d370312cbfa9eaa8a2b3a2b89b622180d3d7630
Martin Otyeka <otyeka.martin@gmail.com>
2018-12-15 22:54:32 -0500

    Add Pod 'Player'

--- Pods/Target Support Files/Pods-Trimmer/Pods-Trimmer-frameworks.sh
+++ Pods/Target Support Files/Pods-Trimmer/Pods-Trimmer-frameworks.sh
@@ -154,10 +154,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/IGListKit/IGListKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Player/Player.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/IGListKit/IGListKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Player/Player.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
